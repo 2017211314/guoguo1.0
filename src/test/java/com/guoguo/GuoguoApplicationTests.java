293fbac2bd52afd3f535b891c9080554b6926290
liuda <DaDa_git>
2018-06-25 18:34:26 +0800

    init project

---
+++ src/test/java/com/guoguo/GuoguoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guoguo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuoguoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
